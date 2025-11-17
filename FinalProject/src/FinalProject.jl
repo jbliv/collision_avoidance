b95301564d4cab274ec36d3478b151664791ad92
jajoshi19 <jajoshi19@gmail.com>
2025-11-16 19:04:39 -0600

    example code runs

---
+++ FinalProject/src/FinalProject.jl
@@ -0,0 +1,10 @@
+module FinalProject
+
+using Ipopt
+using JuMP
+using BlockArrays: BlockArray
+
+include("collision_avoid_game.jl")
+export run
+
+end
---
+++ FinalProject/src/collision_avoid_game.jl
@@ -0,0 +1,34 @@
+
+
+# Steps:
+# cd FinalProject
+# julia
+# ] activate .
+# import Pkg
+# Pkg.instantiate()
+# using FinalProject
+# FinalProject.run()
+
+function run()
+
+    # set up model
+    model = Model(Ipopt.Optimizer)
+
+    # define state vector (S1 pos, S1 vel, S2 pos, S2 vel)
+    @variable(model, x[1:12])
+    x = BlockArray(x, [3, 3, 3, 3])
+
+    # define objective (cost function)
+    @NLobjective(model, Min, x[1]^2 + x[2]^2)
+
+    # define constraints
+    @NLconstraint(model, x[1] + x[2] ≥ 1)
+
+    # solve
+    optimize!(model)
+    value.(x)
+
+end
+
+
+
