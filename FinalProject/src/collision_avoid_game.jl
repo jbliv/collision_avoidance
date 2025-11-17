1a8a0c286c25d087497d060f11fe04608aa7eefd
jajoshi19 <jajoshi19@gmail.com>
2025-11-16 20:16:18 -0600

    set up trajectory game based on HW5 but need orbital dynamics

--- FinalProject/src/collision_avoid_game.jl
+++ FinalProject/src/collision_avoid_game.jl
@@ -9,14 +9,16 @@
 # using FinalProject
 # FinalProject.run()
 
+using FinalProject
+
 function run()
 
     # set up model
     model = Model(Ipopt.Optimizer)
 
     # define state vector (S1 pos, S1 vel, S2 pos, S2 vel)
-    @variable(model, x[1:12])
-    x = BlockArray(x, [3, 3, 3, 3])
+    x̃ = @variable(model, x[1:12])
+    x = BlockArray(x̃, [3, 3, 3, 3])
 
     # define objective (cost function)
     @NLobjective(model, Min, x[1]^2 + x[2]^2)
---
+++ FinalProject/src/trajectory_game.jl
@@ -0,0 +1,59 @@
+
+
+
+
+# set up two player/satellite trajectory game
+# NOTE: currently set to have no environment and allow satellites to move anywhere in 3D space
+function setup_trajectory_game(; environment = nothing)
+
+    # locally define cost for each player
+    cost = let
+        function stage_cost(x, u, t, θ)
+            x1, x2 = blocks(x)
+            u1, u2 = blocks(u)
+
+            # cost for player 1 and player 2
+            # NOTE: currently set to only penalize fuel consumption
+            [
+                norm_sqr(u1),
+                norm_sqr(u2),
+            ]
+
+            # condense into a single cost over time
+            function reducer(stage_costs)
+                reduce(.+, stage_costs) ./ length(stage_costs)
+            end
+
+            TimeSeparableTrajectoryGameCost(stage_cost, reducer, GeneralSumCostStructure(), 1.0)
+
+        end
+
+        function collision_avoid_constraint(xs, us, θ)
+
+            # get collision avoidance constraint for each time (as a vector)
+            mapreduce(vcat, xs) do x
+                x1, x2 = blocks(x)
+
+                # collision avoidance constraint goes here
+                # NOTE: for now, they must be 10 m away from each other
+                (x2[1:3] - x1[1:3])' * (x2[1:3] - x1[1:3]) - 100
+
+            end
+        end
+
+        # orbital dynamics
+        sat_dynamics = orbit_double_integrator(;
+            # NEED TO DEFINE THIS CUSTOM FUNCTION
+        )
+        dynamics = ProductDynamics([sat_dynamics for _ in 1:2])
+
+        TrajectoryGame(dynamics, cost, environment, collision_avoid_constraint)
+
+    end
+
+
+
+
+
+end
+
